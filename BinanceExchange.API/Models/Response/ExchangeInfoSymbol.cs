39bb3ec0e0d2bd677598c99d6add749f04b283fe
Ron Jonkers <ron_jonkers@hotmail.com>
2021-06-22 23:04:42 +0200

    Added some more missing properties to ExchangeInfoSymbol.

--- BinanceExchange.API/Models/Response/ExchangeInfoSymbol.cs
+++ BinanceExchange.API/Models/Response/ExchangeInfoSymbol.cs
@@ -29,25 +29,34 @@ namespace BinanceExchange.API.Models.Response
         public int QuotePrecision { get; set; }
 
         [DataMember(Order = 7)]
+        public int QuoteAssetPrecision { get; set; }
+
+        [DataMember(Order = 8)]
+        public int BaseCommissionPrecision { get; set; }
+
+        [DataMember(Order = 9)]
+        public int QuoteCommissionPrecision { get; set; }
+
+        [DataMember(Order = 10)]
         [JsonProperty(ItemConverterType = typeof(StringEnumConverter))]
         public List<ExchangeInfoOrderType> OrderTypes { get; set; }
 
-        [DataMember(Order = 8)]
+        [DataMember(Order = 11)]
         public bool IcebergAllowed { get; set; }
 
-        [DataMember(Order = 9)]
+        [DataMember(Order = 12)]
         public bool OcoAllowed { get; set; }
 
-        [DataMember(Order = 10)]
+        [DataMember(Order = 13)]
         public bool QuoteOrderQtyMarketAllowed { get; set; }
 
-        [DataMember(Order = 11)]
+        [DataMember(Order = 14)]
         public bool IsSpotTradingAllowed { get; set; }
 
-        [DataMember(Order = 12)]
+        [DataMember(Order = 15)]
         public bool IsMarginTradingAllowed { get; set; }
 
-        [DataMember(Order = 13)]
+        [DataMember(Order = 16)]
         [JsonProperty(ItemConverterType = typeof(ExchangeInfoSymbolFilterConverter))]
         public List<ExchangeInfoSymbolFilter> Filters { get; set; }
     }
